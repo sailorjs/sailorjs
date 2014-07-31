17a6eb23ffa0f861ea0e9960db8aff00daef539b
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-07-31 17:11:32 +0200

    changed npm for git repository

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+/**
+ * Dependencies
+ */
+var Translator = require('sailor-translate');
+var translate = new Translator();
+
+/**
+ * Exports
+ */
 module.exports = {
-  errorify: require("sailor-errorify"),
-  scripts: require("sailor-scripts")
+  errorify  : require("sailor-errorify"),
+  scripts   : require("sailor-scripts"),
+  validator : require("validator"),
+  translate : translate
 };
-
-
