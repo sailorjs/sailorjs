09e7cd18e8cdbcfd3982bdf29f3f1400d8eaed74
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-08-10 22:14:55 +0200

    added silly option

--- bin/sailor.js
+++ bin/sailor.js
@@ -7,13 +7,13 @@ var pkg            = require('../package.json');
 var script         = require('sailor-scripts');
 var parser         = require('nomnom')(pkg);
 var updateNotifier = require('update-notifier');
-var chalk          = require('chalk');
+var chalk           = require('chalk');
 
 /**
  * Initialize
  */
-parser.script("sailor");
-updateNotifier({
+ parser.script("sailor");
+ updateNotifier({
   packageName: pkg.name,
   packageVersion: pkg.version
 }).notify();
@@ -22,69 +22,85 @@ updateNotifier({
 /**
  * version
  */
-parser.option('version', {
+ parser
+ .option('version', {
    abbr: 'v',
    flag: true,
    help: "Output the version of the CLI",
    callback: function() {
     return pkg.name + "@" + pkg.version;
-    }
+  }
 });
 
 /**
  * help
  */
- parser.option('help', {
-    abbr: 'h',
-    flag: true,
-    help: "Ouput this information"
- });
+ parser
+ .option('help', {
+  abbr: 'h',
+  flag: true,
+  help: "Ouput this information"
+});
 
 /**
  * lift command
  */
  parser.command('lift')
-    .callback(function() {
-       script.lift(process.cwd());
-    })
-    .help("Start with a Sailor Proyect");
+ .callback(function(action) {
+  if (action.silly){
+    var opts = {
+      log: {
+        level: 'silly'
+      }
+    };
+    script.lift(process.cwd(), opts);
+  }
+  script.lift(process.cwd());
+})
+ .options({
+  silly: {
+    flag: true,
+    help: "Actiave silly level in logs"
+  }
+})
+ .help("Start with a Sailor Proyect");
 
 /**
  * new command
  */
-parser.command('new')
-   .callback(function(name) {
+ parser.command('new')
+ .callback(function(name) {
 
-    if (name[1]==='module'){
-      if(name[2]===undefined){
-        parser.message("need a name for a new module.", 1);
-      } else {
-        script.newModule(name[2], function(){
-          parser.message("Module '" + chalk.cyan(name[2]) + "' created! ");
-        });
-      }
-      return;
-    }
-
-    if(name[1]===undefined){
-      parser.message("need a name for a new proyect.", 1);
+  if (name[1]==='module'){
+    if(name[2]===undefined){
+      parser.message("need a name for a new module.", 1);
     } else {
-      script.newBase(name[1], function(){
-        parser.message("Proyect '" + chalk.cyan(name[1]) + "' created! ");
+      script.newModule(name[2], function(){
+        parser.message("Module '" + chalk.cyan(name[2]) + "' created! ");
       });
     }
+    return;
+  }
 
-   })
-   .help("Created a new Sailor Proyect");
+  if(name[1]===undefined){
+    parser.message("need a name for a new proyect.", 1);
+  } else {
+    script.newBase(name[1], function(){
+      parser.message("Proyect '" + chalk.cyan(name[1]) + "' created! ");
+    });
+  }
+
+})
+ .help("Created a new Sailor Proyect");
 
 /**
  * new module command
  */
-parser.command('new module')
-   .help("Created a new module for Sailor");
+ parser.command('new module')
+ .help("Created a new module for Sailor");
 
 
 /**
  * Parse!
  */
-parser.parse();
+ parser.parse();
