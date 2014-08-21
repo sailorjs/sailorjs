ca349149e9e91f1f9e41071259eee8d7a791618a
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-08-21 22:01:32 +0200

    Added token support

--- bin/sailor.js
+++ bin/sailor.js
@@ -7,6 +7,7 @@ var pkg            = require('../package.json');
 var script         = require('sailor-scripts');
 var parser         = require('nomnom')(pkg);
 var chalk          = require('chalk');
+var jwt            = require('jwt-simple');
 
 /**
  * Initialize
@@ -41,6 +42,17 @@ var chalk          = require('chalk');
  * lift command
  */
  parser.command('lift')
+ .options({
+   silly: {
+     flag: true,
+     help: "Activate silly level in logs"
+   },
+   production: {
+     flag: true,
+     help: "Activate production environment"
+   }
+ })
+ .help("Start with a Sailor Proyect")
  .callback(function(action) {
   var opts = {};
 
@@ -53,18 +65,50 @@ var chalk          = require('chalk');
     opts.environment = 'production';
 
   script.lift(process.cwd(), opts);
-})
+});
+
+
+
+/**
+ * Create a new token
+ */
+ parser.command('token')
  .options({
-  silly: {
-    flag: true,
-    help: "Activate silly level in logs"
+  algorithm: {
+    abbr: 'a',
+    default: 'HS256',
+    help: 'Supported algorithms: HS256, HS384, HS512 and RS256.'
   },
-  production: {
-    flag: true,
-    help: "Activate production environment"
+  secret: {
+    abbr: 's',
+    help: 'Salt for generate the token'
   }
 })
- .help("Start with a Sailor Proyect");
+ .help("Generate a token for Sailor Proyect")
+ .callback(function(options){
+
+  if (options[1] === undefined ) parser.message("need a payload to generate a token.", 1);
+  if (options.secret === undefined) parser.message("need a secret to generate a token.", 1);
+
+  var payload = options[1];
+  var token = jwt.encode(payload, options.secret);
+
+  parser.message("Token generated with " +
+                 "algorithm " + chalk.underline(options.algorithm) +
+                 ", secret " + chalk.underline(options.secret) +
+                 " and payload " + chalk.underline(payload) +
+                 " is: \n" + token);
+
+  // var payload = { foo: 'bar' };
+  // var secret = 'xxx';
+
+  // // encode
+
+
+  // // decode
+  // var decoded = jwt.decode(token, secret);
+  // console.log(decoded); //=> { foo: 'bar' }
+});
 
 /**
  * new command
@@ -77,7 +121,7 @@ var chalk          = require('chalk');
       parser.message("need a name for a new module.", 1);
     } else {
       script.newModule(name[2], function(){
-        parser.message("Module '" + chalk.cyan(name[2]) + "' created! ");
+        parser.message("Module '" + chalk.cyan(name[2]) + "' created! ", 0);
       });
     }
     return;
@@ -87,7 +131,7 @@ var chalk          = require('chalk');
     parser.message("need a name for a new proyect.", 1);
   } else {
     script.newBase(name[1], function(){
-      parser.message("Proyect '" + chalk.cyan(name[1]) + "' created! ");
+      parser.message("Proyect '" + chalk.cyan(name[1]) + "' created! ", 0);
     });
   }
 
