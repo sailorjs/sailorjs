d892ce0ebcecf2b2e2aad64d9396dc99b90fa15c
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-08-24 11:37:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 /**
- * Dependencies
- */
-var capitalize = function(str){
-  return str[0].toUpperCase() + str.substring(1);
-};
-
-/**
  * Exports
  */
 module.exports = {
   errorify  : require("sailor-errorify"),
   scripts   : require("sailor-scripts"),
-  validator : require("validator"),
+  validator : require("sailor-validator"),
   actionUtil: require("sailor-scripts/node_modules/sails/lib/hooks/blueprints/actionUtil"),
-  translate : require('sailor-translate'),
-  capitalize : capitalize
+  translate : require('sailor-translate')
 };
