fe216a11cd36fa8eefe0858a92632e780f9c1143
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-08-28 17:52:09 +0200

    updated

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * Exports
  */
 module.exports = {
-  errorify  : require("sailor-errorify"),
-  scripts   : require("sailor-scripts"),
-  validator : require("sailor-validator"),
-  actionUtil: require("sailor-scripts/node_modules/sails/lib/hooks/blueprints/actionUtil"),
-  translate : require('sailor-translate')
+  errorify  : require('sailor-errorify'),
+  scripts   : require('sailor-scripts'),
+  validator : require('sailor-validator'),
+  translate : require('sailor-translate'),
+  actionUtil: require('sailor-scripts/node_modules/sails/lib/hooks/blueprints/actionUtil')
 };
