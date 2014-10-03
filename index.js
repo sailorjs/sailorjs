2625b67bb7e48da89b15ee732a7f719b24286bd2
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-10-03 11:43:31 +0200

    added utils in external lib

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ module.exports = {
   scripts   : require('sailor-scripts'),
   validator : require('sailor-validator'),
   translate : require('sailor-translate'),
+  util      : require('./lib/util'),
   actionUtil: require('sailor-scripts/node_modules/sails/lib/hooks/blueprints/actionUtil')
 };
